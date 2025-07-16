02d0cc326820b32ea3bd214e7e9ed1a5ca0f55ab
Yash Tiwari <134519073+yashtiwari7028@users.noreply.github.com>
2025-07-16 21:29:18 +0530

    Update 20221225124206-modify_bookings_add_new_fields.js

--- src/migrations/20221225124206-modify_bookings_add_new_fields.js
+++ src/migrations/20221225124206-modify_bookings_add_new_fields.js
@@ -1,14 +1,9 @@
 'use strict';
 
-/** @type {import('sequelize-cli').Migration} */
+
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    
     await queryInterface.addColumn(
       'Bookings',
       'noOfSeats',
@@ -30,12 +25,7 @@ module.exports = {
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+   
     await queryInterface.removeColumn('Bookings', 'noOfSeats');
     await queryInterface.removeColumn('Bookings', 'totalCost');
 
